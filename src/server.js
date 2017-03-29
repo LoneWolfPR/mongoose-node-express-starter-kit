bfd4dfa9438f2634aab0bc18b362b4b9091a55ce
Paul Romer <promer@allegient.com>
2017-03-29 16:48:58 -0400

    Adds basic example api

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import exampleRoutes from './api/example'; // Replace with real routes
 
 const app = express();
 const port = process.env.PORT || 3000;
+const exampleRouter = exampleRoutes(); // Replace with real router
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use('/example', exampleRouter); // Replace with real router
 
 app.get('/', (req, res) => {
   res.send('Server status: operational...');
